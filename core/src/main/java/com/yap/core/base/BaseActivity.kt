3f578c312d83cb57cea741d9c6f7559072afb450
Faheem Riaz <faheem@digitify.com>
2021-08-02 10:39:54 +0500

    initial project setup

---
+++ core/src/main/java/com/yap/core/base/BaseActivity.kt
@@ -0,0 +1,12 @@
+package com.yap.core.base
+
+import androidx.appcompat.app.AppCompatActivity
+
+
+/**
+Created by Faheem Riaz on 02/08/2021.
+ **/
+
+class BaseActivity : AppCompatActivity() {
+
+}
